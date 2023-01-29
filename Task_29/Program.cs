f0dff15c28908bc1adce22de613c272d4e8afcf0
Yaroslav <rotochenkov@icloud.com>
2023-01-29 12:21:49 +0300

    Now 3/3 ready, yeah

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -3,3 +3,30 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]*/
 
+// Создание рандомного массива
+int [] CreateArray()
+{
+    int [] Array = new int [8];
+    Random number = new Random();
+    for(int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = number.Next(-100,100);
+    }
+    return Array;
+} 
+
+// Вызов Массива
+int [] a = CreateArray();
+
+// Заполнение массива
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array [i]} ");
+    }
+}
+
+// Вызов заполненного массива в консоль
+PrintArray(a);
+
